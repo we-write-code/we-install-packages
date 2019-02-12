2ad3c81bfbc76a54a3acf258a2843e3b38c068a9
Andre Mategka <mategka.andre@gmail.com>
2019-02-12 21:31:38 +0100

    Add RxJS packages

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
-import axios from 'axios'
+import VueRx from 'vue-rx'
+import { Axios } from 'axios-observable'
 
 import App from './App'
 import router from './router'
 import store from './store'
 
+Vue.use(VueRx)
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
-Vue.http = Vue.prototype.$http = axios
+Vue.http = Vue.prototype.$http = Axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
