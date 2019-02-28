16516de5f6e667d04ec0eb3cf111a2659ab0af8d
MXN97 <mx.nothnagel@gmail.com>
2019-02-28 21:47:27 +0100

    Add tentative extraction method for pip to PackageList

--- src/main/packages/PackageList.js
+++ src/main/packages/PackageList.js
@@ -1,5 +1,6 @@
 import Package from './Package'
 import NpmManager from '../packageManagers/NpmManager'
+import PipManager from '../packageManagers/PipManager'
 
 export default class PackageList extends Set {
 
@@ -15,9 +16,30 @@ export default class PackageList extends Set {
       data = JSON.parse(data)
     }
 
-    // TODO: see Package.js/constructor
     for (let key of Object.keys(data)) {
       packageList.add(new Package(key, data[key], NpmManager))
+    }
+
+    return packageList
+  }
+
+  // TODO: Add extraction from referenced dependency files
+  // TODO: Add extraction from setup.py or similar variants
+  static extractFromPip(data) {
+    let packageList = new PackageList()
+
+    for (let line of data) {
+      let data = line.trim()
+
+      // Tentative solution
+      // Explanation of regex:
+      // ^((?:\w+-?)*\w+) -> Package name consisting of groups of characters and digits connected with a hyphen (-)
+      // \s? -> Optional space between package name and version specifier
+      // [!=~<>][=] -> Control characters for version specification
+      // \s? -> Optional space between control characters and version numbers
+      // [^#\s]+ -> Version number (literally: anything besides whitespace and # to ignore trailing comments)
+      let match = new RegExp(/^((?:\w+-?)*\w+)\s?([!=~<>][=]\s?[^#\s]+)?/, 'g').exec(data)
+      if (match) packageList.add(new Package(match[1], match[2], PipManager))
     }
 
     return packageList
