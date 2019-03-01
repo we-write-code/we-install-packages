b88a26ffdb1b0df311adcbcdf9efc5c608f3d941
MXN97 <mx.nothnagel@gmail.com>
2019-03-01 20:52:22 +0100

    Add short circuit assignment to Package constructor

--- src/main/packages/Package.js
+++ src/main/packages/Package.js
@@ -3,7 +3,7 @@ export default class Package {
   // TODO: Clarify if manager subclasses should be stored or "enum" values
   constructor(name, version, packageManager) {
     this.name = name
-    this.version = version
+    this.version = version || ''
     this.packageManager = packageManager
   }
 
